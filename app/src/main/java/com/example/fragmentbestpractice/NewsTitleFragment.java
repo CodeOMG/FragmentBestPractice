3b9bf89bf70fb400390ae6042242088bf023fa34
zhengzhengcool <zhengzhengcool@qq.com>
2022-02-25 17:24:44 +0800

    Initial commit

---
+++ app/src/main/java/com/example/fragmentbestpractice/NewsTitleFragment.java
@@ -0,0 +1,85 @@
+package com.example.fragmentbestpractice;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class NewsTitleFragment extends Fragment {
+    private boolean isTwoPane;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.news_title_frag, container,false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        if (getActivity().findViewById(R.id.news_content_layout) != null) {
+            isTwoPane = true;
+        } else {
+            isTwoPane = false;
+        }
+    }
+
+    class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
+
+        private List<News> mNewsList;
+
+        class ViewHolder extends RecyclerView.ViewHolder {
+
+            TextView newsTitleText;
+
+            public ViewHolder(@NonNull View itemView) {
+                super(itemView);
+                newsTitleText = itemView.findViewById(R.id.news_title);
+            }
+        }
+
+        public NewsAdapter(List<News> mNewsList) {
+            this.mNewsList = mNewsList;
+        }
+
+        @NonNull
+        @Override
+        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.news_item, parent, false);
+            final ViewHolder holder = new ViewHolder(view);
+            view.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    News news = mNewsList.get(holder.getAdapterPosition());
+                    if (isTwoPane) {
+                        NewsContentFragment newsContentFragment = (NewsContentFragment) getFragmentManager().findFragmentById(R.id.news_content_fragment);
+                        newsContentFragment.refresh(news.getTitle(), news.getContent());
+                    } else {
+                        NewsContentActivity.actionStart(getActivity(), news.getTitle(), news.getContent());
+                    }
+                }
+            });
+            return holder;
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull NewsAdapter.ViewHolder holder, int position) {
+            News news = mNewsList.get(position);
+            holder.newsTitleText.setText(news.getTitle());
+        }
+
+        @Override
+        public int getItemCount() {
+            return mNewsList.size();
+        }
+    }
+}
